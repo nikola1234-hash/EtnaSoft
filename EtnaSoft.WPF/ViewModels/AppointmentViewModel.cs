37c6689c6ac1da7ae45f26580bb568bb309f5a17
nikola1234-hash <nikola.savic.beograd@gmail.com>
2021-08-07 14:07:22 +0200

    Started Mapping Custom Fields

--- EtnaSoft.WPF/ViewModels/AppointmentViewModel.cs
+++ EtnaSoft.WPF/ViewModels/AppointmentViewModel.cs
@@ -7,9 +7,11 @@ namespace EtnaSoft.WPF.ViewModels
     public class AppointmentViewModel : AppointmentWindowViewModel
     {
         private AppointmentItem _appointmentItem;
+        public int NumberOfPeople { get; set; }
         public AppointmentViewModel(AppointmentItem appointmentItem, SchedulerControl scheduler = null) : base(appointmentItem, scheduler)
         {
             _appointmentItem = appointmentItem;
+            NumberOfPeople = (int)appointmentItem.CustomFields["NumberOfPeople"];
             RemoveAppointmentCommand = new DelegateCommand(ExecuteRemove);
         }
 
